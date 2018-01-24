853f748e2b4ef54136f4938af22dd6706ce9bde4
horikoshi <horikoshi.seraku@gmail.com>
2018-01-24 12:50:10 +0900

    編集で戻ってきた時の初期値を追記

--- resources/views/employee/regist.blade.php
+++ resources/views/employee/regist.blade.php
@@ -49,7 +49,7 @@
       <div class="col-sm-9 form-group required form-row">
         {!! Form::label('lastName', '社員氏名（名字）', ['class' => 'col-form-label col-sm-3']) !!}
         <!-- <label for="lastName" class="col-form-label col-sm-4">社員氏名（名字）</label> -->
-        {!! Form::text('lastName', null, ['id' => 'lastName', 'class' => 'form-control col-sm-6']) !!}
+        {!! Form::text('lastName', $employee->lastName, ['id' => 'lastName', 'class' => 'form-control col-sm-6']) !!}
         <!-- <input type="text" name="lastName" value="{{ old('lastName') }}" class="form-control col-sm-8"> -->
         <small class="form-text text-muted col-auto">※必須(1-5文字)</small>
       </div>
@@ -58,7 +58,7 @@
       <div class="col-sm-9 form-group required form-row">
         {!! Form::label('firstName', '社員氏名（名前）', ['class' => 'col-form-label col-sm-3']) !!}
         <!-- <label for="firstName" class="control-label">社員氏名（名前）</label> -->
-        {!! Form::text('firstName', null, ['id' => 'firstName', 'class' => 'form-control col-sm-6']) !!}
+        {!! Form::text('firstName', $employee->firstName, ['id' => 'firstName', 'class' => 'form-control col-sm-6']) !!}
         <!-- <input type="text" name="firstName" value="{{ old('firstName') }}" class="form-control"> -->
         <small class="form-text text-muted col-auto">※必須(1-5文字)</small>
       </div>
@@ -67,7 +67,7 @@
       <div class="col-sm-9 form-group required form-row">
         {!! Form::label('lastNameKana', '社員カナ（名字）', ['class' => 'col-form-label col-sm-3']) !!}
         <!-- <label for="lastNameKana" class="control-label">社員カナ（名字）</label> -->
-        {!! Form::text('lastNameKana', null, ['id' => 'lastNameKana', 'class' => 'form-control col-sm-6']) !!}
+        {!! Form::text('lastNameKana', $employee->lastNameKana, ['id' => 'lastNameKana', 'class' => 'form-control col-sm-6']) !!}
         <!-- <input type="text" name="lastNameKana" value="{{ old('lastNameKana') }}" class="form-control"> -->
         <small class="form-text text-muted col-auto">※必須(1-15文字)</small>
       </div>
@@ -76,7 +76,7 @@
       <div class="col-sm-9 form-group required form-row">
         {!! Form::label('firstNameKana', '社員カナ（名前）', ['class' => 'col-form-label col-sm-3']) !!}
         <!-- <label for="firstNameKana" class="control-label">社員カナ（名前）</label> -->
-        {!! Form::text('firstNameKana', null, ['id' => 'firstNameKana', 'class' => 'form-control col-sm-6']) !!}
+        {!! Form::text('firstNameKana', $employee->firstNameKana, ['id' => 'firstNameKana', 'class' => 'form-control col-sm-6']) !!}
         <!-- <input type="text" name="firstNameKana" value="{{ old('firstNameKana') }}" class="form-control"> -->
         <small class="form-text text-muted col-auto">※必須(1-15文字)</small>
       </div>
@@ -85,37 +85,37 @@
       <div class="col-sm-9 form-group required form-row">
         {!! Form::label('department', '所属', ['class' => 'col-form-label col-sm-3']) !!}
         {!! Form::label('department', '部', ['class' => 'col-form-label col-auto']) !!}
-        {!! Form::select('department', ['1' => '1技'], null, ['placeholder' => '', 'id' => 'department', 'class' => 'form-control col-sm-2']) !!}
+        {!! Form::select('department', ['1' => '1技'], $employee->department, ['placeholder' => '', 'id' => 'department', 'class' => 'form-control col-sm-2']) !!}
         <small class="form-text text-muted col-auto">※必須</small>
         {!! Form::label('manager', '課', ['class' => 'col-form-label col-auto']) !!}
-        {!! Form::select('manager', ['1' => '1課', '2' => '2課', '3' => '3課'], null, ['placeholder' => '', 'id' => 'manager', 'class' => 'form-control col-sm-2']) !!}
+        {!! Form::select('manager', ['1' => '1課', '2' => '2課', '3' => '3課'], $employee->manager, ['placeholder' => '', 'id' => 'manager', 'class' => 'form-control col-sm-2']) !!}
         {!! Form::label('sectionChief', '係', ['class' => 'col-form-label col-auto']) !!}
-        {!! Form::select('sectionChief', ['1' => '1係', '2' => '2係', '3' => '3係'], null, ['placeholder' => '', 'id' => 'sectionChief', 'class' => 'form-control col-sm-2']) !!}
+        {!! Form::select('sectionChief', ['1' => '1係', '2' => '2係', '3' => '3係'], $employee->sectionChief, ['placeholder' => '', 'id' => 'sectionChief', 'class' => 'form-control col-sm-2']) !!}
       </div>
 
       <!-- position -->
       <div class="col-sm-9 form-group required form-row">
         {!! Form::label('position', '役職', ['class' => 'col-form-label col-sm-3']) !!}
-        {!! Form::select('position', ['1' => '一般'], null, ['placeholder' => '', 'id' => 'position', 'class' => 'form-control col-sm-3']) !!}
+        {!! Form::select('position', ['1' => '一般'], $employee->position, ['placeholder' => '', 'id' => 'position', 'class' => 'form-control col-sm-3']) !!}
         <small class="form-text text-muted col-auto">※必須</small>
       </div>
 
       <!-- image -->
       <div class="col-sm-9 form-group form-row">
         {!! Form::label('image', '画像', ['class' => 'col-form-label col-sm-3']) !!}
-        {!! Form::url('image', null, ['id' => 'image', 'class' => 'form-control col-sm-6']) !!}
-        <small class="form-text text-muted col-sm-6">※任意(http://, https:// で始まる文字列)</small>
+        {!! Form::url('image', $employee->image, ['id' => 'image', 'class' => 'form-control col-sm-6']) !!}
+        <small class="form-text text-muted col-auto">※任意(http://, https:// で始まる文字列)</small>
       </div>
 
       <!-- comments -->
       <div class="col-sm-9 form-group form-row">
         {!! Form::label('comments', '一言コメント', ['class' => 'col-form-label col-sm-3']) !!}
-        {!! Form::textarea('comments', null, ['id' => 'comments', 'class' => 'form-control col-sm-9', 'rows' => '5']) !!}
+        {!! Form::textarea('comments', $employee->comments, ['id' => 'comments', 'class' => 'form-control col-sm-9', 'rows' => '5']) !!}
         <small class="form-text text-muted col-auto">※任意(300文字まで)</small>
       </div>
 
       <!-- btn -->
-      <div class="col-sm-12 form-group text-center">
+      <div class="col-sm-10 form-group text-center">
         <button type="submit" class="btn btn-primary">登録</button>
       </div>
     {!! Form::close() !!}
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,6 +2,7 @@
 <html lang="ja">
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('title')</title>
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
   </head>
